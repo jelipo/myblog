5ec72459cdeb99ad57e32febbf7ef511809fee7d
frank <frank@mayabot.com>
2018-07-16 23:19:36 +0800

    Delete some thing

--- src/main/java/util/PackingResult.java
+++ src/main/java/util/PackingResult.java
@@ -1,7 +1,5 @@
 package util;
 
-import com.alibaba.fastjson.JSONObject;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,12 +20,7 @@ public class PackingResult {
         return result;
     }
 
-    public static Map toSuccessMap(JSONObject json) {
-        Map result = new HashMap();
-        result.put("resultCode", 200);
-        result.put("data", json);
-        return result;
-    }
+
     public static Map toSuccessMap() {
         Map result = new HashMap();
         result.put("resultCode", 200);
